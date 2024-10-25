4a9ecdea836cdc624d2514b9249a82f893da0f49
Timur Tairov <tairovtk@gmail.com>
2024-10-25 21:25:43 +0300

    commit 2.3

--- components/MyNavbar.tsx
+++ components/MyNavbar.tsx
@@ -17,7 +17,7 @@ const MyNavbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-md md:menu-lg dropdown-content rounded-box z-50 mt-3  w-60 md:w-80 p-2 shadow bg-base-100 dark:bg-base-content border border-gray-400">
+            className="menu menu-md md:menu-lg dropdown-content rounded-box z-50 mt-3 w-60 md:w-80 p-2 shadow bg-base-100 dark:bg-base-content border border-gray-400">
             <Menu />
           </ul>
         </div>
