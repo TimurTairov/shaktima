1e960aebe8bb6bb15abdc4eb90ea99687d03f756
Timur Tairov <tairovtk@gmail.com>
2024-11-10 16:01:16 +0300

    commit 3.13

--- app/seminary/index.js
+++
@@ -1,9 +0,0 @@
-import yogaSiddhovImg from "@/public/img/yoga-siddhov.jpg"
-
-export const seminary = [
-  {
-    id: "yoga-siddhov",
-    title: "Семиинар «Йога Сиддхов» путь к ясности, гармонии и внутреннему покою",
-    img: yogaSiddhovImg,
-  },
-]
