7224ecc890b85511572b964b5af6d9bbdad851d3
Timur Tairov <tairovtk@gmail.com>
2026-01-01 20:26:21 +0300

    commit 1.16 add audiobooks

---
+++ src/helpers/audiobooks.js
@@ -0,0 +1,22 @@
+export const audiobooks = [
+  {
+    id: "004",
+    title: "Освобождающий нектар драгоценных наставлений",
+    url: "/audiobooks/laya-amrita-upadesha-chintamani",
+  },
+  {
+    id: "003",
+    title: "Ишвара Гита",
+    url: "/audiobooks/ishvara-gita",
+  },
+  {
+    id: "002",
+    title: "Авадхута Гита",
+    url: "/audiobooks/avadhuta-gita",
+  },
+  {
+    id: "001",
+    title: "Аштавакра Гита",
+    url: "/audiobooks/ashtavakra-gita",
+  },
+];
