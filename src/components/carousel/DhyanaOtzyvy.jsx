7bb929f55a9a91d04b9bb6135e757956bee8aeb6
Timur Tairov <tairovtk@gmail.com>
2026-07-02 16:21:18 +0300

    commit 1.82

--- src/components/carousel/DhyanaOtzyvy.jsx
+++ src/components/carousel/DhyanaOtzyvy.jsx
@@ -5,7 +5,7 @@ import "react-image-gallery/styles/css/image-gallery.css";
 
 const DhyanaOtzyvy = () => {
   return (
-    <div className="font-light text-gray-700 dark:text-gray-300">
+    <div className="font-light text-gray-700 lg:w-10/12 dark:text-gray-300">
       <h1 className="w-full text-center text-lg font-bold md:text-xl lg:mt-10 lg:text-2xl 2xl:mt-12 dark:text-amber-600">
         Отзывы с прошедших ретритов
       </h1>
