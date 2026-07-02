506bff42481e5b413a4d648aee3e7249cc042bc2
Timur Tairov <tairovtk@gmail.com>
2026-07-02 17:41:52 +0300

    commit 1.83

--- src/components/carousel/DhyanaFoto.jsx
+++ src/components/carousel/DhyanaFoto.jsx
@@ -5,16 +5,14 @@ import "react-image-gallery/styles/css/image-gallery.css";
 
 const DhyanaFoto = () => {
   return (
-    <div className="font-light text-gray-700 lg:w-10/12 dark:text-gray-300">
-      <h1 className="w-full text-center text-lg font-bold md:text-xl lg:mt-10 lg:text-2xl 2xl:mt-12 dark:text-amber-600">
+    <div className="mt-16 text-gray-700 lg:w-10/12 dark:text-gray-300">
+      <h1 className="w-full text-center font-bold dark:text-amber-600">
         Фотографии с предыдущих мероприятий
       </h1>
 
-      <div className="">
-        <div className="my-3">
-          <div className="flex w-full items-center justify-center">
-            <ImageGallery items={photoDhyanaRetreat} {...settings} />
-          </div>
+      <div className="my-1 flex w-full justify-center">
+        <div className="flex w-full items-center justify-center">
+          <ImageGallery items={photoDhyanaRetreat} {...settings} />
         </div>
       </div>
     </div>
--- src/components/carousel/DhyanaOtzyvy.jsx
+++ src/components/carousel/DhyanaOtzyvy.jsx
@@ -5,16 +5,14 @@ import "react-image-gallery/styles/css/image-gallery.css";
 
 const DhyanaOtzyvy = () => {
   return (
-    <div className="font-light text-gray-700 lg:w-10/12 dark:text-gray-300">
-      <h1 className="w-full text-center text-lg font-bold md:text-xl lg:mt-10 lg:text-2xl 2xl:mt-12 dark:text-amber-600">
+    <div className="mt-16 text-gray-700 lg:w-10/12 dark:text-gray-300">
+      <h1 className="w-full text-center font-bold dark:text-amber-600">
         Отзывы с прошедших ретритов
       </h1>
 
-      <div className="">
-        <div className="my-3">
-          <div className="flex w-full items-center justify-center">
-            <ImageGallery items={otzyvyDhyanaRetreat} {...settings} />
-          </div>
+      <div className="my-1 flex w-full justify-center">
+        <div className="flex w-full items-center justify-center">
+          <ImageGallery items={otzyvyDhyanaRetreat} {...settings} />
         </div>
       </div>
     </div>
